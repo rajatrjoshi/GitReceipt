9393bedb6f18f98dfabbf6867e2d2cfb67456022
Rajat Joshi <rajatjoshi@Rajats-MacBook-Air.local>
2025-02-16 16:48:28 +0530

    added application title and faicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import Generate from "@/components/Generate";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App() {
   return <>
+    <Head>
+      <title>Git-Receipt</title>
+      <link
+        rel="icon"
+        href="/icon.svg"
+        type="image/svg"
+        sizes="32x32"
+      />
+    </Head>
     <div className="pt-10 min-h-screen max-w-2xl mx-auto px-4 py-8 sm:py-16">
       <div className="items-center justify-between text-center  pb-9">
         <div className="text-4xl font-bold text-slate-900 pb-2">GitHub Receipt</div>
